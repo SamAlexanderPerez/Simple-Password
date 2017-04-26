9b57d13893d2a6567bef8daa71e040448567d143
SamAlexanderPerez <9sperez@stpatrick.org>
2017-04-26 13:56:36 -0500

    Work on a password checker.

---
+++ simplepass.js
@@ -0,0 +1,21 @@
+//type checkString below
+function checkString(str){
+  if(str.length>=8 && str.length<=20){
+    return str;
+  }
+  else{
+    throw new RangeError("FeelsBadMan");
+  }
+}
+
+
+//type getString below
+function getString(str){
+  try{
+    str=checkString(str);
+  }
+  catch(e){
+    console.log(e.message);
+    getString();
+  }
+}
